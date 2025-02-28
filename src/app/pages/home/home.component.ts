51800cd03b9be1e4fd3614e6e88487c51f281411
julianroldan01 <julian_roldandufet.edu.co>
2025-02-28 15:09:37 -0500

    Agregue CV en sobre mi

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../components/header/header.component";
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
